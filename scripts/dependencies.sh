dc24d1f46dbd3aa0cfaed66d464a55402418fe10
Zhen Lin <greatcarnegie@gmail.com>
2023-05-18 23:27:17 +0000

    add numactl

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,6 +2,6 @@
 
 sudo apt-get install -y openssh-server,curl,tar,gcc,make,g++,libc6-dev,time,strace,less,psmisc
 sudo apt-get install -y build-essential libncurses-dev libssl-dev libelf-dev gcc-8 g++-8 debootstrap qemu-system autotools-dev
-sudo apt-get install -y ipmctl ndctl libtool m4 automake bison byacc flex tree python2 python3 python3-pip xfsprogs fio jq libsnappy-dev libgflags-dev net-tools zstd
+sudo apt-get install -y ipmctl ndctl libtool m4 automake bison byacc flex tree python2 python3 python3-pip xfsprogs fio jq libsnappy-dev libgflags-dev net-tools zstd numactl
 sudo update-alternatives --install /usr/bin/gcc gcc  /usr/bin/gcc-8 1
 sudo pip3 install progress json5 pandas matplotlib numpy
--- share/artifact/filebench.sh
+++ share/artifact/filebench.sh
@@ -3,7 +3,7 @@
 rm -f share/tmp/filebench.log 
 
 # run experiments
-share/fxmark/bin/run-fxmark.py --bench_type filebench --log_dir share/tmp
+numactl -C 0-7 share/fxmark/bin/run-fxmark.py --bench_type filebench --log_dir share/tmp
 
 # # parse log, generate csv
 share/artifact/log_parser.py --log_path share/tmp/filebench.log --config_path share/artifact/log_parser.config --output_path share/output/filebench.csv
